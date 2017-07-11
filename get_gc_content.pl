eb378f0be55745b9032afaf248dcc7757bfc0b49
mrmckain <mrmckain@gmail.com>
2017-07-10 23:56:14 -0500

    Allows all fastq files ending in fq or fastq to be found.

--- get_gc_content.pl
+++ get_gc_content.pl
@@ -1,14 +1,20 @@
 #!/usr/bin/perl -w
 use strict;
 
-open my $file, "<", $ARGV[0];
+
 open my $out, ">", "GC_content.txt";
 print $out "Species\tGC\tAT\tTotal_BP\n";
+my @files = <*.fq>;
+my @files2 = <*.fastq>;
 
+push(@files, @files2);
 
 my $gc;
 my $at;
 my $total_bp;
+for my $filet (@files){
+
+open my $file, "<", $filet;
 while(<$file>){
 	my $id = $_;
 	my $seq = readline($file);
@@ -32,7 +38,7 @@ my $pergc = $gc/($gc+$at);
 my $perat = $at/($gc+$at);
 
 print $out "$id\t$pergc\t$perat\t$total_bp\n";
-}
+
 		
 
 
