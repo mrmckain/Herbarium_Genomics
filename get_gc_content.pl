f38d38a4135f851cda7fd9336397bb4d3331221f
mrmckain <mrmckain@gmail.com>
2017-07-10 23:53:13 -0500

    Fastq updated.

--- get_gc_content.pl
+++ get_gc_content.pl
@@ -4,32 +4,18 @@ use strict;
 open my $file, "<", $ARGV[0];
 open my $out, ">", "GC_content.txt";
 print $out "Species\tGC\tAT\tTotal_BP\n";
-while(<$file>){
-        chomp;
-        my @tarray=split/\s+/;
-        my $tfile;
-        my $dir = "/home/mmckain/Transposome/Prepped_Data/Bootstrap_Replicates_TE_Analysis/" . $tarray[0] . "/";
-        opendir(DH, "$dir");
-        while(my $dfile=readdir(DH)){
-        if($dfile =~ /Ready.fasta|singleend.fasta/){
-        	print "$dfile\n";
-                $tfile=$dir . $dfile;
-     	}
-}
 
-print "$tarray[0]\n";
-my $id = $tarray[0];
-open my $file2, "<", $tfile;
+
 my $gc;
 my $at;
 my $total_bp;
-while(<$file2>){
-	chomp;
-	if(/>/){
-		next;
-	}
-	else{
-		my @tarray =split(//);
+while(<$file>){
+	my $id = $_;
+	my $seq = readline($file);
+	my $plus = readline($file);
+	my $quality = readline($file);
+	chomp($seq);
+		my @tarray =split(//, $seq);
 		$total_bp += scalar @tarray;
 		for my $bp (@tarray){
 			if($bp =~ /A|T/){
